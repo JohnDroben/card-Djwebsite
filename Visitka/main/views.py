7731c895084ad2496f87c8d3b4a9224eb558c97c
DrGorge <drobeng68@gmail.com>
2025-06-07 09:25:55 +0300

    add new code-2

---
+++ Visitka/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+
+
+def home(request):
+    return render (request, 'main/home.html')
+
+def about(request):
+    return render (request, 'main/about.html')
+
+
+
+
+
+
