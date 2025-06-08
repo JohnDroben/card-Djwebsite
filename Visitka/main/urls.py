8e8053fbb350317554a53074e93fe6f7101b1285
DrGorge <drobeng68@gmail.com>
2025-06-08 18:59:50 +0300

    add new code-4

--- Visitka/main/urls.py
+++ Visitka/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('contact/', views.contact, name='contact'),
     path('portfolio/', views.portfolio, name='portfolio'),
 ]
 
--- Visitka/main/views.py
+++ Visitka/main/views.py
@@ -9,11 +9,41 @@ def home(request):
 def about(request):
     return render (request, 'main/about.html')
 
-def contacts(request):
-    return render (request, 'main/contacts.html')
-
 def portfolio(request):
-    return render (request, 'main/portfolio.html')
+    projects = [
+        {
+            'title': 'Проект 1',
+            'description': 'Краткое описание первого проекта',
+            'image': 'project1.jpg',
+            'link': '#',
+            'technologies': ['HTML', 'CSS', 'JavaScript']
+        },
+        {
+            'title': 'Проект 2',
+            'description': 'Краткое описание второго проекта',
+            'image': 'project2.jpg',
+            'link': '#',
+            'technologies': ['Python', 'Django', 'PostgreSQL']
+        },
+        {
+            'title': 'Проект 3',
+            'description': 'Краткое описание третьего проекта',
+            'image': 'project3.jpg',
+            'link': '#',
+            'technologies': ['React', 'Node.js', 'MongoDB']
+        }
+    ]
+    return render(request, 'main/portfolio.html', {'projects': projects})
+
+def contact(request):
+    if request.method == 'POST':
+        # Здесь будет обработка формы
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        # Обычно здесь отправка email или сохранение в БД
+        return render(request, 'main/contact_success.html')
+    return render(request, 'main/contact.html')
 
 
 
