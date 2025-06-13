3b2c971d6cadaf4e70d5465d2cec3177e6337eb9
DrGorge <drobeng68@gmail.com>
2025-06-13 16:53:41 +0300

    add new commit-2

--- Visitka/news/admin.py
+++ Visitka/news/admin.py
@@ -4,8 +4,17 @@ from .models import NewsPost
 class NewsPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date', 'author', 'is_published')
     list_filter = ('is_published', 'pub_date', 'author')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'content', 'excerpt')
     prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'excerpt', 'content', 'image')
+        }),
+        ('Метаданные', {
+            'fields': ('author', 'is_published'),
+            'classes': ('collapse',)
+        }),
+    )
 
 admin.site.register(NewsPost, NewsPostAdmin)
 
--- Visitka/news/models.py
+++ Visitka/news/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 
 
 class NewsPost(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     slug = models.SlugField(max_length=200, unique=True, verbose_name="URL")
+    excerpt = models.CharField("Краткое описание", max_length=300, blank=True)
     content = models.TextField(verbose_name="Содержание")
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     updated_date = models.DateTimeField('Дата обновления', auto_now=True)
@@ -18,6 +20,11 @@ class NewsPost(models.Model):
 
     def get_absolute_url(self):
         return reverse('news_detail', kwargs={'slug': self.slug})
+
+    def get_short_excerpt(self):
+        if self.excerpt:
+            return self.excerpt
+        return Truncator(self.content).chars(150, truncate='...')
 
     class Meta:
         verbose_name = "Новость"
